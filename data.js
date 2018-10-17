4ed89acaad87b8928ddd8c4bf80211aa37e0f457
Desma <dhervey@basecampcodingacademy.org>
2018-10-17 09:29:27 -0500

    Front End Project

---
+++ data.js
@@ -0,0 +1,81 @@
+const PAGE_DATA = {
+    car: [
+        {
+            imageURL:
+                "http://6d6fc150d328228e74ba-78cbc718e4c8c7b0015d47db7822e2aa.r78.cf1.rackcdn.com/SCA665C5XHUX86799/5e417a269cabf7ff3a9037dcc23ce37e.jpg",
+            name: "Rolls Royce 2019",
+            price: 1500,
+            stock: 1,
+            description:
+                "Rolls-Royce Motor Cars Rancho Mirage proudly offers this Brand New 2019 Rolls-Royce Wraith\
+                in Arctic White over a Arctic White/Black interior with 11 miles.The Wraith is powered by a 6.6L\
+                twin turbocharged V12 engine producing 624 hp and 590 lb-ft of torque through a Rear Wheel Drive and\
+                an 8-Speed Automatic Transmission."
+        },
+        {
+            imageURL:
+                "http://mycars2016.com/wp-content/uploads/2019-Nissan-Altima1-660x300.jpg",
+            name: "Altima 2019",
+            description:
+                "This time around the Altima is expected to use Renault’s much newer CMF-CD chassis.\
+                This is a modular platform which would allow Nissan to give the car a much better internal layout.\
+                The car is expected to follow the dimensions of the concept. The 112.2 inch long wheelbase as well\
+                as the 74.4 inches of width are expected to be seen on the production model with minimal differences.\
+                2019 Nissan Altima",
+            price: 1000,
+            stock: 1
+        },
+        {
+            imageURL:
+                "https://686d583e054646acba41-291bf29b6307478a05118664430c5cb6.ssl.cf1.rackcdn.com/WDDSJ4EB9KN702529/397b9251f1bd552ab602b25ef8586189.jpg",
+            name: "Mercedes Benz 2019",
+            description:
+                "Mercedes-Benz calls the CLS a “4-door coupe,”and while other automakers have applied\
+                a coupe descriptor to sedans throughout history, the modern definition still calls for two doors.\
+                Regardless, the CLS is a sensational piece. The new car removes creases and streaks from the body panels,\
+                instead opting for a smoothness that swells and contracts like a Coke bottle. The CLS appears to stretch \
+                from its long hood to its dovetail trunk lid and squat lower than before. The side profile, its most defining\
+                feature with those slim, pillar-less doors and raked glass, has hardly changed since the first generation.\
+                Up front, the CLS grabs the angry race car look of the AMG GT, with a trapezoidal grille and slanted LED headlights.\
+                Out back, flush exhaust tips and squinting LED taillights complete this car’s tailored form.",
+            price: 1300,
+            stock: 2
+        },
+        {
+            imageURL: "https://i.ytimg.com/vi/VoPeFLqw1g0/maxresdefault.jpg",
+            name: "Lamborghini Terzo Millennio 2019",
+            description:
+                "Lamborghini also intends to make the supercapacitors out of carbon-fiber panels that can be\
+                used to form the body of the car, so the Terzo Millennio draws energy from its own body. In other\
+                words: The car itself is the battery.Every detail is designed to make the flow of the wind stream perfect.\
+                Automobili Lamborghini tries to approach the electrification process in an unconventional way, with a concept\
+                car which is radical in the energy management, in the body, in the architecture and in design, in line with Lamborghini culture.",
+            price: 1600,
+            stock: 2
+        },
+        {
+            imageURL:
+                "http://biodrones.ca/wp-content/uploads/2018/02/hqdefault-1441.jpg",
+            name: "Ferrari F80 2019",
+            description:
+                "Ferrari F80 Concept. The Ferrari F80 Concept is a hybrid concept supercar manufactured\
+                by Ferrari and designed by Adriano Raeli. Its body is based on a Formula One car, has a V8 engine and\
+                KERS system that gives the car 1,200 horsepower.",
+            price: 1800,
+            stock: 3
+        },
+        {
+            imageURL:
+                "https://blogmedia.dealerfire.com/wp-content/uploads/sites/380/2018/06/2019-Jaguar-XJ50-Limited-Edition-Rear_o.jpg",
+            name: "Jaquar XJ50 2019",
+            description:
+                "When you slide into the driver’s seat of the 2019 Jaguar XJ50 Limited Edition you will have a choice\
+                between a 340-horsepower 3.0-liter supercharged V-6 engine that can be paired with rear-wheel drive or\
+                all-wheel drive and a power-hungry 470-horsepower 5.0-liter supercharged V-8 with rear-wheel drive. Jaguar XJ\
+                models powered by the 5.0-liter powertrain will sprint from zero to 60 mph in a mind-blowing 4.9 seconds. Jaguar XJ50\
+                models will feature an eight-speed automatic transmission.",
+            price: 1700,
+            stock: 3
+        }
+    ]
+};
